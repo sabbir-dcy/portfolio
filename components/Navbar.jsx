0d5ad24998e86b7ad34bcb5a0ef125ed7f256644
sabbir-dcy <sabbirahmd.bracu@gmail.com>
2022-06-10 13:29:48 +0600

    users

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,6 +12,7 @@ const Navbar = () => {
         <Link href={'#skills'}>Skills</Link>
         <Link href={'#projects'}>Projects</Link>
         <Link href={'#contact'}>Contact</Link>
+        <Link href={'/about'}>About</Link>
       </div>
     </div>
   )
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const about = () => {
+const about = ({ data: users }) => {
+
   return (
-    <div>about</div>
+    <div>{users.length}</div>
   )
 }
 
+export async function getServerSideProps(context) {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+  return {
+    props: { data },
+  }
+}
 export default about
